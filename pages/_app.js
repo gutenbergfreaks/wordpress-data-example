d4e696c1c8eefa3fd8bda5858b472f1d53bae21d
miminari <mimitips@gmail.com>
2020-11-28 11:51:31 +0900

    Fix GTM error

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,9 @@ import TagManager from 'react-gtm-module'
 import '../styles/globals.css'
 import '../store'
 
-const tagManagerArgs = {
-  gtmid: 'GTM-NF2RJV9'
-}
-
 class MyApp extends App {
   componentDidMount() {
-    TagManager.initialize(tagManagerArgs)
+    TagManager.initialize({ gtmId: 'GTM-NF2RJV9' })
   }
   render() {
     const { Component, pageProps } = this.props
